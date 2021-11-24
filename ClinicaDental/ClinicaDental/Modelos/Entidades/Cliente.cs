5a97faa4c97c2bb1137e006eff96705826d8c563
Emerson-medina <eomedinam@unah.hn>
2021-11-23 23:46:53 -0600

    Proyecto v1
    Hicimos una prueba para conectar con la base de datos

---
+++ ClinicaDental/ClinicaDental/Modelos/Entidades/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicaDental.Modelos.Entidades
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Genero { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+
+    }
+}
