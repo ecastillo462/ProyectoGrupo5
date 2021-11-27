648c05af861b4c611790939a2e344f4dd1dcafb2
samyrsierra <samyr.sierra@unah.hn>
2021-11-26 23:59:20 -0600

    Update Servicios.cs

--- ClinicaDental/ClinicaDental/Modelos/Entidades/Servicios.cs
+++ ClinicaDental/ClinicaDental/Modelos/Entidades/Servicios.cs
@@ -10,8 +10,9 @@ namespace ClinicaDental.Modelos.Entidades
     {
         public int Id { get; set; }
 
-        public int Id { get; set; }
-
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Costo { get; set; }
 
 
 
